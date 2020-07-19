5833e88d4300d4983c329652f409069e24b8758b
Hannes Achleitner <hannes.software@gmx.at>
2020-07-19 06:47:42 +0200

    Github app updater

--- android/mainSample/src/main/java/com/google/mlkit/MLMainActivity.kt
+++ android/mainSample/src/main/java/com/google/mlkit/MLMainActivity.kt
@@ -2,6 +2,7 @@ package com.google.mlkit
 
 import android.os.Bundle
 import javax.annotation.Nonnull
+import info.hannes.github.AppUpdateHelper
 
 class MLMainActivity : NavigationActivity() {
     override fun onCreate(@Nonnull savedInstanceState: Bundle?) {
@@ -11,5 +12,12 @@ class MLMainActivity : NavigationActivity() {
                 .beginTransaction()
                 .add(R.id.contentInfo, SystemInfoFragment())
                 .commit()
+
+        AppUpdateHelper.checkForNewVersion(
+                this,
+                BuildConfig.GIT_USER,
+                BuildConfig.GIT_REPOSITORY,
+                BuildConfig.VERSION_NAME
+        )
     }
 }
