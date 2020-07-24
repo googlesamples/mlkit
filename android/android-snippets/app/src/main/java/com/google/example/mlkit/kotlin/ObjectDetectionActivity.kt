f7afdb9bae79d9571f2393348606357e8d6345e9
CY <steven-chengji-yan@users.noreply.github.com>
2020-07-24 16:04:42 -0700

    Add code snippet for object detection and tracking features

--- android/android-snippets/app/src/main/java/com/google/example/mlkit/kotlin/ObjectDetectionActivity.kt
+++ android/android-snippets/app/src/main/java/com/google/example/mlkit/kotlin/ObjectDetectionActivity.kt
@@ -16,58 +16,45 @@
 
 package com.google.example.mlkit.kotlin
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
+import com.google.mlkit.common.model.LocalModel
 import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.label.ImageLabeler
-import com.google.mlkit.vision.label.ImageLabeling
-import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
+import com.google.mlkit.vision.objects.DetectedObject
+import com.google.mlkit.vision.objects.ObjectDetection
+import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions
+import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions
+import com.google.mlkit.vision.objects.defaults.PredefinedCategory
 
 class ObjectDetectionActivity : AppCompatActivity() {
 
-    private fun labelImages(image: InputImage) {
-        val options = ImageLabelerOptions.Builder()
-                .setConfidenceThreshold(0.8f)
+    private fun useDefaultObjectDetector() {
+        // [START create_default_options]
+        // Live detection and tracking
+        var options = ObjectDetectorOptions.Builder()
+                .setDetectorMode(ObjectDetectorOptions.STREAM_MODE)
+                .enableClassification()  // Optional
                 .build()
 
-        val labeler = ImageLabeling.getClient(options)
+        // Multiple object detection in static images
+        options = ObjectDetectorOptions.Builder()
+                .setDetectorMode(ObjectDetectorOptions.SINGLE_IMAGE_MODE)
+                .enableMultipleObjects()
+                .enableClassification()  // Optional
+                .build()
+        // [END create_default_options]
 
-        // [START run_detector]
-        val result = labeler.process(image)
-                .addOnSuccessListener { labels ->
-                    // Task completed successfully
-                    // [START_EXCLUDE]
-                    // [START get_labels]
-                    for (label in labels) {
-                        val text = label.text
-                        val confidence = label.confidence
-                    }
-                    // [END get_labels]
-                    // [END_EXCLUDE]
-                }
-                .addOnFailureListener { e ->
-                    // Task failed with an exception
-                    // ...
-                }
-        // [END run_detector]
-    }
+        // [START create_detector]
+        val objectDetector = ObjectDetection.getClient(options)
+        // [END create_detector]
 
-    private fun configureAndRunImageLabeler(image: InputImage) {
-        // [START on_device_image_labeler]
-        // To use default options:
-        val labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
+        val image = InputImage.fromBitmap(
+                Bitmap.createBitmap(IntArray(100 * 100), 100, 100, Bitmap.Config.ARGB_8888),
+                0)
 
-        // Or, to set the minimum confidence required:
-        // val options = ImageLabelerOptions.Builder()
-        //     .setConfidenceThreshold(0.7f)
-        //     .build()
-        // val labeler = ImageLabeling.getClient(options)
-
-        // [END on_device_image_labeler]
-
-        // Process image with custom onSuccess() example
         // [START process_image]
-        labeler.process(image)
-                .addOnSuccessListener { labels ->
+        objectDetector.process(image)
+                .addOnSuccessListener { results ->
                     // Task completed successfully
                     // ...
                 }
@@ -77,16 +64,71 @@ class ObjectDetectionActivity : AppCompatActivity() {
                 }
         // [END process_image]
 
-        // Process image with example onSuccess()
-        labeler.process(image)
-                .addOnSuccessListener { labels ->
-                    // [START get_image_label_info]
-                    for (label in labels) {
-                        val text = label.text
-                        val confidence = label.confidence
-                        val index = label.index
-                    }
-                    // [END get_image_label_info]
+        val results = listOf<DetectedObject>()
+        // [START read_results_default]
+        for (detectedObject in results) {
+            val boundingBox = detectedObject.boundingBox
+            val trackingId = detectedObject.trackingId
+            for (label in detectedObject.labels) {
+                val text = label.text
+                if (PredefinedCategory.FOOD == text) {
+                    // ...
                 }
+                val index = label.index
+                if (PredefinedCategory.FOOD_INDEX == index) {
+                    // ...
+                }
+                val confidence = label.confidence
+            }
+        }
+        // [END read_results_default]
+    }
+
+    private fun useCustomObjectDetector() {
+        val image = InputImage.fromBitmap(
+                Bitmap.createBitmap(IntArray(100 * 100), 100, 100, Bitmap.Config.ARGB_8888),
+                0)
+
+        // [START create_local_model]
+        val localModel =
+                LocalModel.Builder()
+                        .setAssetFilePath("asset_file_path_to_tflite_model")
+                        // or .setAbsoluteFilePath("absolute_file_path_to_tflite_model")
+                        .build()
+        // [END create_local_model]
+
+        // [START create_custom_options]
+        // Live detection and tracking
+        var options =
+                CustomObjectDetectorOptions.Builder(localModel)
+                        .setDetectorMode(CustomObjectDetectorOptions.STREAM_MODE)
+                        .enableClassification()
+                        .setClassificationConfidenceThreshold(0.5f)
+                        .setMaxPerObjectLabelCount(3)
+                        .build()
+
+        // Multiple object detection in static images
+        options =
+                CustomObjectDetectorOptions.Builder(localModel)
+                        .setDetectorMode(CustomObjectDetectorOptions.SINGLE_IMAGE_MODE)
+                        .enableMultipleObjects()
+                        .enableClassification()
+                        .setClassificationConfidenceThreshold(0.5f)
+                        .setMaxPerObjectLabelCount(3)
+                        .build()
+        // [END create_custom_options]
+
+        val results = listOf<DetectedObject>()
+        // [START read_results_custom]
+        for (detectedObject in results) {
+            val boundingBox = detectedObject.boundingBox
+            val trackingId = detectedObject.trackingId
+            for (label in detectedObject.labels) {
+                val text = label.text
+                val index = label.index
+                val confidence = label.confidence
+            }
+        }
+        // [END read_results_custom]
     }
 }
