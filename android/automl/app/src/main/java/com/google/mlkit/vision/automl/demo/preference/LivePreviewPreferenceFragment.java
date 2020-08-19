8a8218bbbe80494dbae0b1777d6cb6f157590081
Hannes Achleitner <hannes.software@gmx.at>
2020-08-19 07:33:01 +0200

    Rename to avoid conflicts

--- android/automl/app/src/main/java/com/google/mlkit/vision/automl/demo/preference/LivePreviewPreferenceFragment.java
+++ android/automl/app/src/main/java/com/google/mlkit/vision/automl/demo/preference/LivePreviewPreferenceFragment.java
@@ -45,7 +45,7 @@ public class LivePreviewPreferenceFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.preference_live_preview);
+        addPreferencesFromResource(R.xml.preference_live_preview_automl);
         setUpCameraPreferences();
         setUpRemoteModelNamePreferences();
     }
--- android/translate/app/src/main/java/com/google/mlkit/samples/translate/kotlin/MainActivity.kt
+++ android/translate/app/src/main/java/com/google/mlkit/samples/translate/kotlin/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_translate_main)
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
