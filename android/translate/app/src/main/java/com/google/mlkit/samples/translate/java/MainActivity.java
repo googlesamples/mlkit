e0b2f8175c4f48e981efd3a8715a4b3435d5c5e5
Yi Zhou <deanzhou@google.com>
2020-08-21 14:42:33 -0700

    Pull in external request https://github.com/googlesamples/mlkit/pull/106/commits/8a8218bbbe80494dbae0b1777d6cb6f157590081
    Change-Id: I6ffc924e20ab578c9ba968400e756b1b37c08d4a

--- android/translate/app/src/main/java/com/google/mlkit/samples/translate/java/MainActivity.java
+++ android/translate/app/src/main/java/com/google/mlkit/samples/translate/java/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_translate_main);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
