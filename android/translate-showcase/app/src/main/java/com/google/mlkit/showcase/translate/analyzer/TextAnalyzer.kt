645184f93a09db8aeaaa6276acd020eb6f7a58e2
Yi Zhou <deanzhou@google.com>
2022-01-13 23:10:17 +0000

    Version update for translate showcase.
    Change-Id: I041a87bf6ed078393fdb68130e340add67995885

--- android/translate-showcase/app/src/main/java/com/google/mlkit/showcase/translate/analyzer/TextAnalyzer.kt
+++ android/translate-showcase/app/src/main/java/com/google/mlkit/showcase/translate/analyzer/TextAnalyzer.kt
@@ -31,7 +31,7 @@ import com.google.mlkit.showcase.translate.util.ImageUtils
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.TextRecognizerOptions
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import java.util.concurrent.Executor
 
 /**
