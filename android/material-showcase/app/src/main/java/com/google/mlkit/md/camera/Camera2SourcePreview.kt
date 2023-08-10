4499902c78997833fad4f9154ecd86e4d91770a9
Eeshan Jamal <eeshan.jamal@gmail.com>
2023-08-10 22:23:37 +0530

    Fixed the camera preview appearance size by applying center crop scaling.

--- android/material-showcase/app/src/main/java/com/google/mlkit/md/camera/Camera2SourcePreview.kt
+++ android/material-showcase/app/src/main/java/com/google/mlkit/md/camera/Camera2SourcePreview.kt
@@ -25,7 +25,8 @@ import android.view.SurfaceView
 import android.widget.FrameLayout
 import com.google.mlkit.md.R
 import com.google.mlkit.md.Utils
-import java.io.IOException
+import kotlin.math.abs
+import kotlin.math.roundToInt
 
 /** Preview the camera image in the screen.  */
 class Camera2SourcePreview(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
@@ -106,32 +107,31 @@ class Camera2SourcePreview(context: Context, attrs: AttributeSet) : FrameLayout(
             } else {
                 size.width.toFloat() / size.height
             }
-        } ?: layoutWidth.toFloat() / layoutHeight.toFloat()
+        } ?: (layoutWidth.toFloat() / layoutHeight.toFloat())
 
-        // Match the width of the child view to its parent.
-        val childHeight = (layoutWidth / previewSizeRatio).toInt()
-        if (childHeight <= layoutHeight) {
-            for (i in 0 until childCount) {
-                getChildAt(i).layout(0, 0, layoutWidth, childHeight)
-            }
+        //Calculate the new surface view size by scaling the layout width/height based on aspect ratio
+        val newLayoutWidth: Int
+        val newLayoutHeight: Int
+        if (width < height * previewSizeRatio) {
+            newLayoutHeight = height
+            newLayoutWidth = (height * previewSizeRatio).roundToInt()
         } else {
-            // When the child view is too tall to be fitted in its parent: If the child view is
-            // static overlay view container (contains views such as bottom prompt chip), we apply
-            // the size of the parent view to it. Otherwise, we offset the top/bottom position
-            // equally to position it in the center of the parent.
-            val excessLenInHalf = (childHeight - layoutHeight) / 2
-            for (i in 0 until childCount) {
-                val childView = getChildAt(i)
-                when (childView.id) {
-                    R.id.static_overlay_container -> {
-                        childView.layout(0, 0, layoutWidth, layoutHeight)
-                    }
-                    else -> {
-                        childView.layout(
-                            0, -excessLenInHalf, layoutWidth, layoutHeight + excessLenInHalf
-                        )
-                    }
-                }
+            newLayoutWidth = width
+            newLayoutHeight = (width / previewSizeRatio).roundToInt()
+        }
+
+        //Apply the new width & height to surface view only in a way that it should center crop the camera preview
+        val excessWidthInHalf = abs(newLayoutWidth - layoutWidth) / 2
+        val excessHeightInHalf = abs(newLayoutHeight - layoutHeight) / 2
+        surfaceView.layout(
+            -excessWidthInHalf, -excessHeightInHalf, newLayoutWidth, newLayoutHeight
+        )
+
+        //Apply the actual layout width & height to rest of its child views
+        for (i in 0 until childCount) {
+            val childView = getChildAt(i)
+            if (!childView.equals(surfaceView)){
+                childView.layout(0, 0, layoutWidth, layoutHeight)
             }
         }
 
